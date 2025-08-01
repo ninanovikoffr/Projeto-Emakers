663e53fd3904e0d5a49ca0833e1dd118d80a5290
ninanovikoffr <151045142+ninanovikoffr@users.noreply.github.com>
2025-08-01 14:06:42 -0300

    ajustes tela registro

--- emakers/src/pages/Registrar.jsx
+++ emakers/src/pages/Registrar.jsx
@@ -3,6 +3,7 @@ import img from '../assets/imagemgrande.svg';
 import './Registrar.css' // Importando o CSS específico da página
 import Input from '../componentes2/Input.jsx' // Importando o componente de Input
 import '../componentes2/Input.css' // Importando o CSS específico do componente
+import setaimagem from '../assets/seta.svg'; // Importando a imagem da seta
  
 
 function Registrar (){
@@ -28,15 +29,16 @@ function Registrar (){
             <div>
                 <Input placeholder="Confirmar Senha" type="password" />
             </div>
+            <div>
+                <button className="seta">  <img src={setaimagem} alt="Seta"/> </button>
+            </div>
 
-        </div>
-        
-        <div className="botaofazerlogin">
-            <a href="/login" className="fazerLogin"> Fazer Login</a>
+            <button href="/login" className="fazerLogin"> Fazer Login</button>
+
         </div>
 
         <div className="imagemGrande"> 
-            <img src={img} alt="iamgem ilustrativa do site"/>
+            <img src={img} alt="imagem ilustrativa do site"/>
         </div>
 
         </div>
